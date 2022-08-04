ccbd2e837a18ba5fb0f0b1961680d23e0a089050
VYWL <dksu40@gmail.com>
2022-08-04 23:31:34 +0900

    add) pillList, nutList 데이터 추가

--- dataset/index.js
+++ dataset/index.js
@@ -1,94 +1,14 @@
-const fs = require("fs");
-const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-const { useAxios } = require("./hook");
-const fakeUA = require("fake-useragent");
-const cheerio = require("cheerio");
-const url = require("url");
-const { getPillData } = require("./services/vimeal");
-const { getAllSearchResult } = require("./services/immcoach");
+const fs = require('fs');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const { useAxios } = require('./hook');
+const fakeUA = require('fake-useragent');
+const cheerio = require('cheerio');
+const url = require('url');
+const { getPillData } = require('./services/vimeal');
+const { getAllSearchResult } = require('./services/immcoach');
+const { getProductInfo } = require('./services/aimee');
 puppeteer.use(StealthPlugin());
-console.clear();
+// console.clear();
 
-const getReviewHtml = async (productId, page) => {
-	const url = "https://www.coupang.com/vp/product/reviews/attachment/images";
-
-	const params = {
-		productId,
-		page,
-		viRoleCode: 3,
-		size: 300,
-	};
-
-	const headers = {
-		accept: "*/*",
-		"accept-language": "ko-KR,ko;q=0.9",
-		"sec-fetch-dest": "empty",
-		"sec-fetch-mode": "cors",
-		"sec-fetch-site": "same-origin",
-		"sec-gpc": "1",
-		"x-requested-with": "",
-		cookie: "sid=0fb25153df8f463fa4e31ba991c1330b10ef32ad; PCID=24808129754131946590222; MARKETID=24808129754131946590222; x-coupang-accept-language=ko_KR; X-CP-PT-locale=ko_KR; x-coupang-origin-region=KOREA; bm_sz=C8CCC1177C0F40A4F68BFEFBFD7CF2C9~YAAQzZc7F9YttfyBAQAA2h8HABBwApAGgMl+ooUPovCy20kzrmvoAORq0+7NZkn2kejR12boKfTkyA6jiXFybi3GAwjjSrhmF8lFeKQrVXDnYWzXNY54WX4Tk44WDj8YHC8Dy3VItdpX2WDq27M3Nc7i0PX2gkR1/+V73MIXUcak/ug7mp6zBygKHOqI48z1zRwo4VrIka20vTMJKuSDXgnxgIemdyqc9aRSYe2Nyl+fQSDGRCstkulBtot0kWQmBGc8zJ6l85ie9klgUtNJvNkO5ivpzJEvLl+qlPxWrb8pTHS8~4408645~3617861; _fbp=fb.1.1657857840995.1616477302; _abck=23C85272CC7B8BB7196A53B81C0D4240~0~YAAQzZc7Fzs9tfyBAQAADVAHAAhb4qKiKkSj3+0fY9vWLcla/Z+19s1J6g5U51kcwunX2P5+A0m9mzoNr2r0DcYHdWRpMIktMPgF/4mWHH0D2rELyWfRtjEF4NgkwAwO/Su4G0outwOK+PPnBVyKXhHzqdD+WhGvqVTlDd1g8BywfIke3mull4WKalXUxsgRC7IEtTLxBm5ryfjWCQcP3975V1p4tPz+A+u3UdYu/TGBDLB9KY7cyJzygZQSbux+x+ju6VhQ069ExsbyHmJhZFYkT5PiVFv1qcO2HFykXJmGqPWBgPJsylSzhwv63sxckXOnfm/Uu27AAmsLic1vJ0zhMBaYbCfFNW5u0h7+8fBO5CIt0KIqvUC711U2ZiY52kFUz6sYhc6JU9LzQIraA1wZ77R/dAwbeA==~-1~-1~-1; overrideAbTestGroup=%5B%5D; bm_mi=79F5A4D42BC50A6E853C8A9086278EB2~YAAQvgI1F2j1AviBAQAA0keGABDNjXauuWc5/v+McIuaPLAM2zR7hPf241rtxI4tRMRHdjWFO+FpyHEdQyhdnVrqyg+ylELsqf+M9uelF60BdHCk9XPQaq+Pqmb/83jU02cmDvFtfJkmzY69RY4WKJ79HhWaJ21hE1z3a1iAcvrRQEaDaPdLq2QzXSS3Q14Kugwa+ZJLPLnuUO2B+xJuQKUcN0DNCTTmDKaIvjB83AZBgc22MhCDEl9hx4+XlE2h3MEbRbuJR8o/KHY7Tb+3GFt5mHjpBfM2Lt6GzgLdVzCBVg5tyNpBun2lbtUJtXexOVOoce3sdOTsmi8aIBATqJZvVyyIqfLE1zAc2njKOQLpABcIF+I=~1; baby-isWide=small; ak_bmsc=EF6E5DF74A8B6A5708329DB0B5D98A1A~000000000000000000000000000000~YAAQvgI1FzgNA/iBAQAA+4WGABAj/5qOgW4OSU+Qs9deXCRzxcmAlH4MplT/LYx8tzUp9+5WY1FUnkiTl4zOSJGnv4NxEdvZCsZoGLeP+kQgtT+0eCCcCDctna+s1AzzPtF7StgCo3O0p1pGrfRxQ0oz78M1W950s/ppkuoPFvRr+4+ClEWDuZTJdrLakD7d6slwEtgmjyPjSs5fEBqxPG1lOXpNZlIbWnUQaVj+49Of+LFQE7IcT8EKK6S8EHqfMEmkZIQpAhYiGUH5qctVGPnJAJHWdRqnMDKtoqlpJ1swYnpB3NMOfzZnuxIYlOXdQkU48zAxlFNjBJNTm7bU4aVNxfyPhXIU5lXoMOkTtBBXAxIEa1BL7d2mmBt67gJBG9T/+La3rRl8C7+lnpgCPzQr2C7OFyBMICNiJRvViViblH2injyDyR50Y5cnFTVKOKAZonfi05zGcu1pQ5RqdaXGyPSPP2mDQZX/ntZ+JQMY4zLrrf8tPX0BUMNRl/q2BD1Er+B7D0SPrcKY0JRwOfW4aTpOf66kOyYaW/lngGXg3iWNAaU3GIhj; bm_sv=242DD308FBFD226F61D73A7A55FBDD5F~YAAQvgI1F7gNA/iBAQAAPoeGABBwPkaqyNrRadAI5O/8SPMTAQ3ShFBJYcFjAzZ/1gsH7zygwHy7fDNdSkvaEg3nm5163dSqUiIQkBKxACpoygeodDviu94nUXijz9NeUBpOwm2jHPBShbLGfoH5VYq5aj7Dnk+SJOibmATyPCK44K5iyZNWMUBmIAJjwiFQS/KHpfMkQMqAKpD5PIrhrQFEwHkXFMDYk2osLT8ht7Lo09VFfLW6XoVH0Jdw6lFPxw==~1",
-		Referer: `https://www.coupang.com/vp/products/${productId}?itemId=${productId}&vendorItemId=3549112936&sourceType=CATEGORY&categoryId=493019&isAddedCart=`,
-		"Referrer-Policy": "strict-origin-when-cross-origin",
-	};
-
-	const { responseData } = await useAxios({
-		url,
-		params,
-		headers,
-	});
-
-	return responseData;
-};
-
-const getReviewImgUrl = async (productId) => {
-	const result = [];
-
-	let page = 1;
-
-	while (1) {
-		const html = await getReviewHtml(productId, page++); // fs.readFileSync("./result_1.html", "utf8");
-
-		const $ = await cheerio.load(html);
-
-		if ($("div.sdp-review__gallery__section__list__not-category").length) break;
-
-		const elemSelector = "li.sdp-review__gallery__section__list__item";
-		$(elemSelector).each((i, e) => {
-			const { "data-reviewid": rid, src: rawLink } = $("img", e).attr();
-			const { protocol, path, host } = url.parse(rawLink);
-
-			const pathContents = path.split("/");
-
-			const refined_path = pathContents.map((e) => (e === "320" ? "q-1" : e)).join("/");
-
-			result.push(`${protocol}//${host}${refined_path}`);
-		});
-	}
-
-	console.log(" 끝");
-
-	fs.writeFileSync(`./output/coupang/reviewImg/${productId}.json`, JSON.stringify(result), "utf8");
-	// return result;
-};
-
-(async () => {
-	const temp = JSON.parse(fs.readFileSync("./output/immcoach/result.json", "utf8"));
-
-	console.log(temp[0]);
-	fs.writeFileSync("./temp.html", temp[0], "utf8");
-
-	return;
-	const list = JSON.parse(fs.readFileSync("./data/coupang/refinedResult.json", "utf8"));
-
-	for (let idx = 162; idx < list.length; ++idx) {
-		const { name, productId } = list[idx];
-
-		if (!productId) return;
-
-		process.stdout.write(`${name} (id:${productId}) TASK`);
-
-		const temp = await getReviewImgUrl(productId);
-	}
-})();
+(async () => {})();
--- dataset/services/aimee/index.js
+++ dataset/services/aimee/index.js
@@ -1,51 +1,116 @@
-const { useAxios } = require("../../hook");
-const fakeUa = require("fake-useragent");
-const categories = JSON.parse(fs.readFileSync("./categories.json", "utf8"));
+const { useAxios } = require('../../hook');
+const fs = require('fs');
+const fakeUa = require('fake-useragent');
 
-const getPillDataByCategory = async (searchWord) => {
-	const fetchURL = "https://aimee.kr/resources/functions/autocomplete.php";
+const CATRGORY_URL = 'https://aimee.kr/resources/functions/autocomplete.php';
+const PRODUCT_URL = 'https://energybalanceaimee.kr/api/product_info/';
 
-	const { responseData } = await useAxios({
-		url: fetchURL,
-		data: {
-			search_text: searchWord,
-			$path_type: "비교하기",
-		},
-		method: "POST",
-		headers: { "Content-Type": "multipart/form-data", "User-Agent": fakeUa() },
-	});
+const getPillDataByCategory = async searchWord => {
+    const fetchURL = CATRGORY_URL;
 
-	return responseData;
+    const { responseData } = await useAxios({
+        url: fetchURL,
+        data: {
+            search_text: searchWord,
+            $path_type: '비교하기',
+        },
+        method: 'POST',
+        headers: { 'Content-Type': 'multipart/form-data', 'User-Agent': fakeUa() },
+    });
+
+    return responseData;
 };
 
 function sleep(ms) {
-	const wakeUpTime = Date.now() + ms;
-	while (Date.now() < wakeUpTime) {}
+    const wakeUpTime = Date.now() + ms;
+    while (Date.now() < wakeUpTime) {}
 }
 
 const getResult = async (start, end) => {
-	let result = [];
+    const categories = JSON.parse(fs.readFileSync('./categories.json', 'utf8'));
+    let result = [];
 
-	for (let i = start; i < end; ++i) {
-		const keyword = categories[i];
+    for (let i = start; i < end; ++i) {
+        const keyword = categories[i];
 
-		const res = await getPillDataByCategory(keyword);
+        const res = await getPillDataByCategory(keyword);
 
-		result.push({ key: keyword, data: res });
-	}
+        result.push({ key: keyword, data: res });
+    }
 
-	fs.writeFileSync(`./data/searchResult_${start}-${end}.json`, JSON.stringify(result), "utf8");
+    fs.writeFileSync(`./data/searchResult_${start}-${end}.json`, JSON.stringify(result), 'utf8');
 };
 
 const getAllSearchResult = async () => {
-	for (let i = 0; i < categories.length; i += 100) {
-		await getResult(i, i + 100);
+    const categories = JSON.parse(fs.readFileSync('./categories.json', 'utf8'));
+    for (let i = 0; i < categories.length; i += 100) {
+        await getResult(i, i + 100);
 
-		console.log(`${i + 1} - ${i + 101} complete`);
-		sleep(10000);
-	}
+        console.log(`${i + 1} - ${i + 101} complete`);
+        sleep(10000);
+    }
+};
+
+const getProductInfo = async idx => {
+    const fetchURL = PRODUCT_URL + `${idx}`;
+
+    const { responseData } = await useAxios({
+        url: fetchURL,
+        headers: { 'Content-Type': 'multipart/form-data', 'User-Agent': fakeUa() },
+    });
+
+    return responseData;
+};
+
+function sleep(sec) {
+    return new Promise(resolve => setTimeout(resolve, sec * 1000));
+} // 함수정의
+
+const getAllProductInfo = async () => {
+    const pillList = JSON.parse(fs.readFileSync('./data/aimee/pillList.json', 'utf8'))['plusName'];
+
+    const real_pill = pillList.filter(e => e['name']);
+
+    real_pill.sort((a, b) => a.idx - b.idx);
+
+    const result1 = [];
+    const result2 = [];
+    const result3 = [];
+    try {
+        while (real_pill.length) {
+            const proms = [];
+            const target = real_pill.splice(0, 100);
+
+            target.forEach(e => {
+                const { idx } = e;
+                proms.push(getProductInfo(idx));
+            });
+
+            const res = await Promise.all(proms);
+            if (real_pill.length < 30000 && real_pill.length >= 20000) result1.push(...res);
+            if (real_pill.length < 20000 && real_pill.length >= 10000) result2.push(...res);
+            if (real_pill.length < 10000) result3.push(...res);
+
+            console.log(
+                `100개 처리 완료. ${result1.length + result2.length + result3.length}개 저장됨.  ${
+                    real_pill.length
+                }개 남음`
+            );
+            await sleep(5);
+        }
+    } catch (e) {
+        console.log(`오류로 중단됨. ${result.length}개 저장됨.  ${real_pill.length}개 남음`);
+        fs.writeFileSync('./test1.json', JSON.stringify(result1), 'utf8');
+        fs.writeFileSync('./test2.json', JSON.stringify(result2), 'utf8');
+        fs.writeFileSync('./test3.json', JSON.stringify(result3), 'utf8');
+    }
+
+    fs.writeFileSync('./test1.json', JSON.stringify(result1), 'utf8');
+    fs.writeFileSync('./test2.json', JSON.stringify(result2), 'utf8');
+    fs.writeFileSync('./test3.json', JSON.stringify(result3), 'utf8');
 };
 
 module.exports = {
-	getPillDataByCategory,
+    getPillDataByCategory,
+    getProductInfo,
 };
